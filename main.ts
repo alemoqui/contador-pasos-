3bb6dc60196695f3bd01212f5431ec9fa335f368
alemoqui <130452678+alemoqui@users.noreply.github.com>
2023-05-23 11:54:02 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.clearScreen()
+    basic.showNumber(paso * 0.67)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(paso)
+})
+input.onButtonPressed(Button.B, function () {
+    paso = 0
+    basic.showNumber(0)
+})
+let paso = 0
+paso = 0
+basic.showNumber(0)
 basic.forever(function () {
-	
+    if (input.acceleration(Dimension.X) > 1300) {
+        paso += 1
+    }
 })
